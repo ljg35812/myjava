34eeae20ce0943b1fe7073032c8548e087f8a6a1
ljg35812 <ljg35812@gmail.com>
2025-03-18 19:26:25 +0900

    변경

---
+++ src/main/java/Bj9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bj9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String username = sc.next();
+        System.out.println(username + "??!");
+
+        sc.close();
+
+    }
+}
