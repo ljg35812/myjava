aecb0b85378cd5e0bfae6ff3d5d72318f871c251
data8320-05 <ljg35812@gmail.com>
2025-03-19 13:17:56 +0900

    변경

---
+++ src/main/java/Bj10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bj10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        a = sc.nextInt();
+        b = a - 543;
+
+        System.out.println(b);
+
+        sc.close();
+    }
+}
