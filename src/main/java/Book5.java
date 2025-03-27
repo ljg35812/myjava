6d70377e19643d8f5e385d575c63df6ecc75442b
data8320-05 <ljg35812@gmail.com>
2025-03-27 18:41:19 +0900

    변경

---
+++ src/main/java/Book5.java
@@ -0,0 +1,17 @@
+public class Book5 {
+    public static void main(String[] args) {
+
+
+        int a = 0;
+
+        a  -= 900 * 10;
+        a += 1800 * 2;
+        a += 3500 * 5;
+        a += 4000 * 4;
+        a += 1500;
+        a += 2000 * 4;
+        a += 1800 * 5;
+
+        System.out.println("오늘의 총매출액은 " + a + "원입니다");
+    }
+}
