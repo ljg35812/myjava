f9a63332b11848ef671b2b5d3a4d42ff27c73d99
data8320-05 <ljg35812@gmail.com>
2025-03-28 20:07:53 +0900

    변경

---
+++ src/main/java/Nohello18.java
@@ -0,0 +1,11 @@
+public class Nohello18 {
+    public static void main(String[] args) {
+        String ss = "Frist java를 공부중입니다.";
+        String var1, var2;
+
+        var1 = ss.toUpperCase();
+        System.out.println(var1);
+        var2 = ss.toLowerCase();
+        System.out.println(var2);
+    }
+}
