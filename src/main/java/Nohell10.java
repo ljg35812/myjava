46b5f140a63666e348c7cc44586603843ca3b26c
data8320-05 <ljg35812@gmail.com>
2025-03-28 20:05:29 +0900

    변경

---
+++ src/main/java/Nohell10.java
@@ -0,0 +1,13 @@
+public class Nohell10 {
+    public static void main(String[] args) {
+
+        int num = 20;
+        num++ ; System.out.print(num+" ");
+        num-- ; System.out.print(num+" ");
+        num += 3 ; System.out.print(num + " ");
+        num -= 3 ; System.out.print(num + " ");
+        num *= 3 ; System.out.print(num + " ");
+        num /= 3 ; System.out.print(num + " ");
+        num %= 3 ; System.out.print(num + " ");
+    }
+}
