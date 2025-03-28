4d2a0eadeea8ea66c367431ca26defb9c70a09ad
data8320-05 <ljg35812@gmail.com>
2025-03-28 20:07:09 +0900

    변경

---
+++ src/main/java/Nohello15.java
@@ -0,0 +1,8 @@
+public class Nohello15 {
+    public static void main(String[] args) {
+        String var1 = "난생처음 \n" +
+                      "자바\n" +
+                      "열공중입니다.";
+        System.out.println(var1);
+    }
+}
