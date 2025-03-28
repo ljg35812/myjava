c1a33384bebc9ca20c356f070acb495dab13206d
data8320-05 <ljg35812@gmail.com>
2025-03-28 20:07:25 +0900

    변경

---
+++ src/main/java/Nohello16.java
@@ -0,0 +1,11 @@
+public class Nohello16 {
+    public static void main(String[] args) {
+        String str = "난샘처음 자바";
+        int len;
+
+        len = str.length();
+
+        System.out.println("문자열 : " + str);
+        System.out.println("문자열 길이 : " + len);
+    }
+}
