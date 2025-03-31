86efd7ec89a369f443525df331cd5f024d2b8f56
data8320-05 <ljg35812@gmail.com>
2025-03-31 21:35:34 +0900

    변경

---
+++ src/main/java/Lebook2.java
@@ -0,0 +1,16 @@
+public class Lebook2 {
+    public static void main(String[] args) {
+        double a, b, c, g;
+
+        int d = 3;
+        int e = 2;
+        int f = 1;
+
+        a = 3.5;
+        b = 4.0;
+        c = 4.5;
+
+        g = ((d * a) + (e * b) + (f * c)) / (d + e + f);
+        System.out.println("평균학점: " + g);
+    }
+}
