afd0b45196b49c6d460e005b64f110475cbd75e2
data8320-05 <ljg35812@gmail.com>
2025-03-31 21:35:44 +0900

    변경

---
+++ src/main/java/Lebook5.java
@@ -0,0 +1,12 @@
+public class Lebook5 {
+    public static void main(String[] args) {
+        String a = "first java를 공부 중입니다.";
+        String b, c;
+
+        b = a.toUpperCase();
+        System.out.println(b);
+        c = a.toLowerCase();
+        System.out.println(c);
+
+    }
+}
