8c218cf671a181810e5b92055f38e49c960b6ddc
data8320-05 <ljg35812@gmail.com>
2025-04-01 21:39:35 +0900

    변경

--- src/main/java/Bj27.java
+++ src/main/java/Bj27.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Bj27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (b == 1 || b == 2) {
+            System.out.println("NEWBIE");
+        } else if (b <=a) {
+            System.out.println("OlDBIE");
+        } else {
+            System.out.println("TlE!");
+        }
+        sc.close();
+    }
 }
