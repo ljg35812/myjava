92a3cd4db4fbcd11197f837c08624dae07dde8f0
data8320-05 <ljg35812@gmail.com>
2025-04-04 17:04:15 +0900

    변경

---
+++ src/main/java/NOhell023.java
@@ -0,0 +1,9 @@
+public class NOhell023 {
+    public static void manin(String[] args) {
+        int a = 99;
+
+        if (a < 100) {
+            System.out.println("100보다 작습니다.");
+        }
+    }
+}
