82a12967a4caca46c8b948648a27aa8f2f847e01
data8320-05 <ljg35812@gmail.com>
2025-04-04 15:44:31 +0900

    변경

---
+++ src/main/java/Bye10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bye10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        String c = (a + b).replaceAll("\\s","").toLowerCase();
+        System.out.println(c);
+        sc.close();
+    }
+}
