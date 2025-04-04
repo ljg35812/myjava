1b97272496eefd3e2791f050e048bdfd10e29722
data8320-05 <ljg35812@gmail.com>
2025-04-04 17:26:12 +0900

    변경

---
+++ src/main/java/Nohello30.java
@@ -0,0 +1,7 @@
+public class Nohello30 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i + ": 난생처음 자바는 재미있습니다. ^^");
+        }
+    }
+}
