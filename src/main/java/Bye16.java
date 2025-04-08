d47cf11abfd95f6254685feecdc45afe0be3cb5d
data8320-05 <ljg35812@gmail.com>
2025-04-08 22:00:52 +0900

    변경

---
+++ src/main/java/Bye16.java
@@ -0,0 +1,10 @@
+public class Bye16 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        for (int i = 0; i <= 100; i += 2) {
+            a += i;
+        }
+        System.out.println(a);
+    }
+}
