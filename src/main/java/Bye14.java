9f1fb5378f89c4bd9a0deb256ed5ffe3f1174799
data8320-05 <ljg35812@gmail.com>
2025-04-08 22:00:26 +0900

    변경

---
+++ src/main/java/Bye14.java
@@ -0,0 +1,11 @@
+public class Bye14 {
+    public static void main(String[] args) {
+        int i;
+        for ( i = 1; i <= 5; i++) {
+            System.out.println(i + "번째반복");
+        }
+        for ( i = 5; i >= 1; i--) {
+            System.out.println(i + "번 말할거야");
+        }
+    }
+}
