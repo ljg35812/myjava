a407d7c4a8b4aec489217673bfeada71c82a369b
data8320-05 <ljg35812@gmail.com>
2025-04-08 22:00:39 +0900

    변경

---
+++ src/main/java/Bye15.java
@@ -0,0 +1,10 @@
+public class Bye15 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        for(int i = 1; i <= 100; i++) {
+            a += i;
+        }
+        System.out.println(a);
+    }
+}
