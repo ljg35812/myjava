f2855cf3b7c9723299dce29a2290f551359775ec
data8320-05 <ljg35812@gmail.com>
2025-04-08 22:01:21 +0900

    변경

---
+++ src/main/java/Bye18.java
@@ -0,0 +1,13 @@
+public class Bye18 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <=9; i++) {
+            System.out.println("___________");
+            for (int j = 1; j <=9; j++) {
+                int a = i*j;
+                System.out.println(i + " x " + j + " = " + a);
+            }
+        }
+        System.out.println();
+    }
+}
