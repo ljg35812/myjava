3ede32f0f7d3300ff636b8e53b37a303651298b0
data8320-05 <ljg35812@gmail.com>
2025-04-11 16:55:27 +0900

    변경

---
+++ src/main/java/Nohello35.java
@@ -0,0 +1,11 @@
+public class Nohello35 {
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 9; ++a) {
+            System.out.println("-----------");
+            for (int b = 1; b <= 9; ++b) {
+                System.out.println(a + " * " + b + " = " + (a * b));
+            }
+        }
+    }
+}
