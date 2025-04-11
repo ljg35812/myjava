7ec200c56d4b657c31dae80118490c1d7d924046
data8320-05 <ljg35812@gmail.com>
2025-04-11 16:55:07 +0900

    변경

---
+++ src/main/java/Nohello34.java
@@ -0,0 +1,10 @@
+public class Nohello34 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        for (int b = 1; b <= 1000; b++) {
+            a = a + b;
+        }
+        System.out.println("1부터 1000까지의 합계: " + a);
+    }
+}
