f3d63afac4c237d2163a1b1e4fd1d8bbdd68fb72
data8320-05 <ljg35812@gmail.com>
2025-04-14 10:29:18 +0900

    변경

--- src/main/java/Lebj33.java
+++ src/main/java/Lebj33.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Lebj33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i =4; i <= a; i += 4) {
+            System.out.print("long ");
+        }
+        System.out.print("int");
+        sc.close();
+    }
 }
