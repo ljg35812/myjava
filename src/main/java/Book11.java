fa7acdc8b59ad1f4d8f00d9f022ecd23ebfd64fa
data8320-05 <ljg35812@gmail.com>
2025-04-15 20:58:06 +0900

    변경

---
+++ src/main/java/Book11.java
@@ -0,0 +1,14 @@
+class Rabbit3 {
+    public String shape;
+    public int xPos;
+    public int yPos;
+}
+
+public class Book11 {
+    public static void main(String[] args) {
+        Rabbit3 rabbit = new Rabbit3();
+
+        rabbit.xPos = 100;
+        rabbit.yPos = 200;
+    }
+}
