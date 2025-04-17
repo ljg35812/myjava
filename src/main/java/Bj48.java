39aa9583b29ce6278a2c26f60b4ec85f65985952
data8320-05 <ljg35812@gmail.com>
2025-04-17 16:43:22 +0900

    변경

--- src/main/java/Bj48.java
+++ src/main/java/Bj48.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class Bj48 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int a = sc.nextInt();
 
+        m += a;
+        if (m > 59) {
+            h += m / 60;
+            m %= 60;
+        }
+
+        if (h > 23) {
+            d += h / 24;
+            h %= 24;
+        }
+        System.out.println(d + "d : " + h + "h : " + m + "m");
     }
 }
