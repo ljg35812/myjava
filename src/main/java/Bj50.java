b84ea220d59d6e3c847fd33a8c4ecce955f85f5f
data8320-05 <ljg35812@gmail.com>
2025-04-17 16:42:54 +0900

    변경

--- src/main/java/Bj50.java
+++ src/main/java/Bj50.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Bj50 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int []b = new int[a];
+
+        int c = Integer.MAX_VALUE;
+        int d = Integer.MIN_VALUE;
+
+        for (int i =1; i <= a; i++) {
+            b[i - 1] = sc.nextInt();
+
+            if (b[i - 1] < c) {
+                c = b[i - 1];
+            }
+            if (b[i - 1] > d) {
+                d = b[i - 1];
+            }
+        }
+        System.out.println(c + " " + d);
+
+        sc.close();
+    }
 }
