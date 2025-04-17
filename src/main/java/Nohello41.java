498f51d447295a6241dbfaf09266242901281145
data8320-05 <ljg35812@gmail.com>
2025-04-17 19:17:46 +0900

    변경

---
+++ src/main/java/Nohello41.java
@@ -0,0 +1,21 @@
+class Rabbit5 {
+    private String shape;
+    private int xPos;
+    private int yPos;
+    public int number;
+
+    void setPosition(int xPos, int yPos) {
+        xPos = xPos;
+        yPos = yPos;
+    }
+}
+
+public class Nohello41 {
+    public static void main(String[] args) {
+        Rabbit5 rabbit = new Rabbit5();
+
+        rabbit.setPosition(100, 200);
+
+        rabbit.number = 1;
+    }
+}
