860fcba2b326455cbd230abace5694c991c8f62b
data8320-05 <ljg35812@gmail.com>
2025-04-17 16:43:11 +0900

    변경

--- src/main/java/Bj49.java
+++ src/main/java/Bj49.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Bj49 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String b;
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < a - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
