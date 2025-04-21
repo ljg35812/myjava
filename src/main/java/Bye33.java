c2213fb543175382dcd9b17f733e58768cce4891
data8320-05 <ljg35812@gmail.com>
2025-04-21 17:13:53 +0900

    변경

---
+++ src/main/java/Bye33.java
@@ -0,0 +1,14 @@
+public class Bye33 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 300;
+        int c = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 5 == 0 || i % 7 == 0) {
+                c += i;
+            }
+        }
+        System.out.println(c);
+    }
+}
