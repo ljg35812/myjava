700361c537ad13c76b90d483b3a82a15d3baf0ad
data8320-05 <ljg35812@gmail.com>
2025-04-24 19:43:57 +0900

    변경

---
+++ src/main/java/Bye46.java
@@ -0,0 +1,14 @@
+public class Bye46 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 300;
+        int c = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 5 == 0 || i % 7 == 0) {
+                c += i;
+            }
+        }
+        System.out.println(c);
+    }
+}
