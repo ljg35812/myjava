4d460101107e9d9d2e6a4b3225f886c475c05c45
data8320-05 <ljg35812@gmail.com>
2025-05-07 16:45:02 +0900

    변경

--- src/main/java/Bye55.java
+++ src/main/java/Bye55.java
@@ -1,17 +1,16 @@
-abstract class Fish {
+abstract class Fish6 {
     abstract void swim();
 }
-class GoldFish extends Fish {
+
+class GoldFish6 extends Fish6 {
     @Override
     void swim() {
         System.out.println("헤엄치고 있습니다.");
     }
 }
-
-public class Bye29 {
+public class Bye55 {
     public static void main(String[] args) {
-        GoldFish gf = new GoldFish();
-
+        GoldFish6 gf = new GoldFish6();
         gf.swim();
     }
 }
