c19b0f13d930aabe4490d57aa9f4e96be2eac712
data8320-05 <ljg35812@gmail.com>
2025-05-08 16:08:32 +0900

    변경

---
+++ src/main/java/Lebj43.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lebj43 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int []a = new int[n];
+        int e, d;
+
+        for (int i = 0; i < n; i++) {
+            a[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            e = b - 1;
+            d = c - 1;
+        }
+    }
+}
