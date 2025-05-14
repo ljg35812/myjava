8810f6bbc67fea2215d23822f8559c2befd7ab72
data8320-05 <ljg35812@gmail.com>
2025-05-14 15:38:48 +0900

    변경

---
+++ src/main/java/Lebj52.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lebj52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> ls = new ArrayList<Integer>();
+        int a = sc.nextInt();
+        int b = Integer.MIN_VALUE;
+        int c = 0;
+
+        for (int i = 0; i < a; i++) {
+            ls.add(sc.nextInt());
+            if (ls.get(i) > b) {
+                b = ls.get(i);
+                c = i + 1;
+            }
+        }
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
