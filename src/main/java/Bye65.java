fd9244af55e2bf90a76cbebc003bd53824fd18e2
data8320-05 <ljg35812@gmail.com>
2025-05-16 23:36:28 +0900

    변경

---
+++ src/main/java/Bye65.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Bye65 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Map<String, String>> ls = new ArrayList<>();
+        Map<String, String> map = null;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("이름: ");
+            String name = sc.nextLine();
+
+            System.out.print("이메일: ");
+            String mail = sc.nextLine();
+
+            System.out.print("부서: ");
+            String dept = sc.nextLine();
+
+            map = new HashMap<>();
+
+            map.put("name", name);
+            map.put("mail", mail);
+            map.put("dept", dept);
+
+            ls.add(map);
+
+            map = null;
+        }
+        int lsstsize = ls.size();
+
+        System.out.println(lsstsize);
+
+        for (Map<String, String> m : ls) {
+            System.out.println(m.get("name"));
+            System.out.println(m.get("mail"));
+            System.out.println(m.get("dept"));
+            System.out.println("-----------");
+        }
+
+        sc.close();
+    }
+}
