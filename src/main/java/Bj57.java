312b349c29fbb5b7035d49659ce093186e2f4f38
data8320-05 <ljg35812@gmail.com>
2025-05-21 15:09:25 +0900

    변경

---
+++ src/main/java/Bj57.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bj57 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int b = a.length();
+        System.out.println(b);
+
+        sc.close();
+    }
+}
