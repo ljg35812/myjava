ec260705207557ea80f5d822badbf85fcef68dc1
data8320-05 <ljg35812@gmail.com>
2025-05-21 15:10:34 +0900

    변경

--- src/main/java/Bj60.java
+++ src/main/java/Bj60.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class Lebj32 {
+public class Bj60 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        String b = sc.next();
+        int c = 0;
 
-
-        for (int i = 1; i <= a; i++) {
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-
-            System.out.println(b + c);
+        for (int i = 0; i < a; i++) {
+            c += b.charAt(i) - '0';
         }
+        System.out.println(c);
+
         sc.close();
     }
 }
