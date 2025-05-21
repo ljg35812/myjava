12c2f728dc0696c10821cfe3ffb854fd6ff080ec
data8320-05 <ljg35812@gmail.com>
2025-05-21 13:17:09 +0900

    변경

--- src/main/java/Bj56.java
+++ src/main/java/Bj56.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class Bye8 {
+public class Bj56 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String a = sc.nextLine();
-        String b = sc.nextLine();
-
-        String c = (a + b).replaceAll("\\s","").toLowerCase();
-
+        int b = sc.nextInt();
+        char c = a.charAt(b);
         System.out.println(c);
+
+        sc.close();
     }
 }
