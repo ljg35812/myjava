895038999849fd0a642fa840741a53208f992d1a
data8320-05 <ljg35812@gmail.com>
2025-05-21 20:20:30 +0900

    변경

---
+++ src/main/java/Bye69.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bye69 {
+    public static void main(String[] args) {
+        List<Integer> ls = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            ls.add(i + 1);
+        }
+
+        for (int a : ls) {
+            System.out.print(a + " ");
+        }
+    }
+}
