d5b054fb7795519102fea13b0ebc42f4f9f4b548
data8320-05 <ljg35812@gmail.com>
2025-05-21 20:20:45 +0900

    변경

---
+++ src/main/java/Bye70.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Bye70 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> ls = new ArrayList<>();
+
+        System.out.println("저장할 데이터수 입력");
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            ls.add(sc.nextInt());
+        }
+        for (int b : ls) {
+            System.out.print(b + " ");
+        }
+    }
+}
