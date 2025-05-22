15c3c7a94a4cf7b0a7649e978b9f01f2ac95f3f8
data8320-05 <ljg35812@gmail.com>
2025-05-22 19:48:48 +0900

    변경

---
+++ src/main/java/Lebook7.java
@@ -0,0 +1,11 @@
+public class Lebook7 {
+    static String a (String s1, String s2) {
+        String b = s1 + s2;
+        return b;
+    }
+
+    public static void main (String[] args) {
+        String c = a("appple", " pie");
+        System.out.println(c);
+    }
+}
