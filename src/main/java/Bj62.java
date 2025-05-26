42194111fe3a340a40054ae129fb9c9ab7e79f8e
data8320-05 <ljg35812@gmail.com>
2025-05-26 17:20:56 +0900

    변경

--- src/main/java/Bj62.java
+++ src/main/java/Bj62.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
-public class Bj61 {
+public class Bj62 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
+        int b = a.split(" ").length;
 
+        System.out.println(b);
     }
 }
+
