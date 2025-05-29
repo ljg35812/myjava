8b5f0d4316f40854597aa3fd7414bdfe8cff04d0
data8320-05 <ljg35812@gmail.com>
2025-05-29 12:14:59 +0900

    변경

---
+++ src/main/java/Bj66.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Bj66 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        a = a.replaceAll("\\s+", "");
+
+        for (int i = 0; i < a.length(); i++) {
+            char b = a.charAt(i);
+        }
+    }
+}
