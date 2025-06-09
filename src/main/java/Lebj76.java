b450f010bb7d468befec2ef9cb1eabdc4f6e3cfe
data8320-05 <ljg35812@gmail.com>
2025-06-09 17:59:45 +0900

    변경

---
+++ src/main/java/Lebj76.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Lebj76 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Character a = sc.next().charAt(0);
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
